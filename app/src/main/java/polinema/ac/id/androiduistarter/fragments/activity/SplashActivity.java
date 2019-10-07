fb8dd96e94e8cae371706ed81fb2a2d3ccc49ec7
FauzanAr <faurinta12345@gmail.com>
2019-10-07 14:50:14 +0800

    creating new layout and changin manifest to splash screen

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activity/SplashActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activity/SplashActivity.java
@@ -1,4 +1,4 @@
-package polinema.ac.id.androiduistarter.activities;
+package polinema.ac.id.androiduistarter.fragments.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import polinema.ac.id.androiduistarter.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
